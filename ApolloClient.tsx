5ff9e14435bb2886535fb6b2c5b57b0875e745c0
HuanTH <huan.huy.truong@gmail.com>
2023-11-08 17:05:06 +0700

    Add .env

--- ApolloClient.tsx
+++ ApolloClient.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: "https://trungluffy.live/graphql",
+    uri: process.env.NEXT_PUBLIC_URL_API_BACKEND,
     cache: new InMemoryCache(),
   });
 };
